78c8f3f6b868db92f8ff26c1b4bdf1151b036cb2
Drayton <drayton.kittel@gmail.com>
2019-02-28 16:38:54 -0600

    adding new file extractShifts.js

---
+++ extractShifts.js
@@ -0,0 +1,10 @@
+shifts['shiftID'] = row_values[0]
+shifts['shiftName'] = row_values[1]
+shifts['stationCode'] = row_values[2]
+shifts['startTime'] = to8601(row_values[4])
+shifts['endTime'] = to8601(row_values[5])
+shifts['notes'] = row_values[6]
+shifts['crewOne'] = row_values[15]
+shifts['crewOneReplaced'] = row_values[16]
+shifts['crewTwo'] = row_values[20]
+shifts['crewTwoReplaced'] = row_values[21]
