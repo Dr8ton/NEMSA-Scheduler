bb7b9fd732284ff6236f5dd9ba35f3aa880987c4
Drayton <drayton.kittel@gmail.com>
2019-02-28 17:04:16 -0600

    stopped work

--- extractShifts.js
+++ extractShifts.js
@@ -1,10 +1,25 @@
-shifts['shiftID'] = row_values[0]
-shifts['shiftName'] = row_values[1]
-shifts['stationCode'] = row_values[2]
-shifts['startTime'] = to8601(row_values[4])
-shifts['endTime'] = to8601(row_values[5])
-shifts['notes'] = row_values[6]
-shifts['crewOne'] = row_values[15]
-shifts['crewOneReplaced'] = row_values[16]
-shifts['crewTwo'] = row_values[20]
-shifts['crewTwoReplaced'] = row_values[21]
+
+import Excel from 'exceljs';
+
+const book = [];
+workbook.getWorksheet(
+    ( sheet => {
+  const sheet = [];
+  worksheet.eachRow(row => {
+    sheet.push(row.values);
+  });
+book.push(sheet);
+});
+
+https://github.com/exceljs/exceljs#access-worksheets
+
+// shifts['shiftID'] = row_values[0]
+// shifts['shiftName'] = row_values[1]
+// shifts['stationCode'] = row_values[2]
+// shifts['startTime'] = to8601(row_values[4])
+// shifts['endTime'] = to8601(row_values[5])
+// shifts['notes'] = row_values[6]
+// shifts['crewOne'] = row_values[15]
+// shifts['crewOneReplaced'] = row_values[16]
+// shifts['crewTwo'] = row_values[20]
+// shifts['crewTwoReplaced'] = row_values[21]
