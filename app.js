0fec58a43642f8a17556029a8d1d47a0e01973e4
Drayton <drayton.kittel@gmail.com>
2019-02-28 16:27:44 -0600

    finished the code to download file from crew scheduler.

--- app.js
+++ app.js
@@ -25,12 +25,32 @@ let generalReportURL = 'https://scheduling.acadian.com/CrewScheduler/ReportsCrys
 
     await page.waitForSelector('#ddlReport');
     await page.select('#ddlReport', '313');
+    console.log('report set ');
 
     await page.waitForSelector('#ddlRegion');
     await page.select('#ddlRegion', '9');
+    console.log('region set ');
+
+    await page.waitForSelector('#rbDates');
+    await page.click('#rbDates');
+    console.log('dates set');
+
+
+    let today = new Date();
+    let nextYear = new Date(today.getFullYear()+1, today.getMonth(), today.getDay());
+
+    await page.waitForSelector('#dpStart_dateInput');
+    console.log('start date found');
+    await page.type('#dpStart_dateInput', today.toLocaleDateString());
+    await page.type('#dpEnd_dateInput', nextYear.toLocaleDateString());
+
+    await page.waitForSelector('#btnExport');
+    await page.click('#btnExport');
+    console.log('export btn clicked');
+
+    const response = await page.waitForNavigation({waitUntil:'networkidle2'});
+    console.log("load finished")
+
 })();
 
-let d = new Date();
-console.log('d = ' + d); 
-let nextYear = new Date(d.getFullYear()+1, d.getMonth(), d.getDay());
-console.log('nextYear = ' + nextYear);
+
