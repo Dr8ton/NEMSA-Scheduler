02efceef61e6ac68fb0e1c48fc3338bf141995c2
dr8ton <drayton.kittel@gmail.com>
2019-03-07 23:06:23 +0000

    added functions for extracting excel report

--- extract.js
+++ extract.js
@@ -1,6 +1,37 @@
 var xlsx = require('node-xlsx').default;
 
 const workSheetsFromFile = xlsx.parse(`Daily.xlsx`);
-console.log(workSheetsFromFile[0].data);
+console.log(workSheetsFromFile[0].data[4][9]);
 
 // TODO: work the spreadsheet
+
+//  [ 4980781,
+//    '301: Day O (12hr std crew)',
+//    'CAD',
+//    43520,
+//    43520.25,
+//    43520.75,
+//    <3 empty items>,
+//    true,
+//    '301',
+//    <4 empty items>,
+//    '018124 Primeaux, M.',
+//    <4 empty items>,
+//    '018263 Valencia, C.',
+//    <14 empty items>,
+//    3,
+//    0,
+//    0,
+//    0 ],
+
+function isActiveParamedicPreceptor(){
+    
+}
+
+function isActiveEMTPreceptor(){
+    
+}
+
+function isSprintTruck(){
+    
+}
