655e6d3bf2785814980a1251c8800c5301105a0d
Drayton Kittel <Drayton.kittel@gmail.com>
2019-04-01 16:53:16 -0500

    added functions getAllActiveEmtPreceptors() and getAllActiveParamedicPreceptors()

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,17 +3,17 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
-  var config = {
-    apiKey: "AIzaSyAt79Jd_GIWsjGKBJkX4nRjd0AlmVfkS2Y",
-    authDomain: "nemsa-6c408.firebaseapp.com",
-    databaseURL: "https://nemsa-6c408.firebaseio.com",
-    projectId: "nemsa-6c408",
-    storageBucket: "nemsa-6c408.appspot.com",
-    messagingSenderId: "512173906413"
-  }
-  firebase.initializeApp(config); 
-  
-  var db = firebase.firestore();
+var config = {
+  apiKey: "AIzaSyAt79Jd_GIWsjGKBJkX4nRjd0AlmVfkS2Y",
+  authDomain: "nemsa-6c408.firebaseapp.com",
+  databaseURL: "https://nemsa-6c408.firebaseio.com",
+  projectId: "nemsa-6c408",
+  storageBucket: "nemsa-6c408.appspot.com",
+  messagingSenderId: "512173906413"
+}
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
 
 
 
@@ -27,16 +27,16 @@ require("firebase/firestore");
  * 
  * @returns {?} TODO: what does this method return???
  */
-export function addParamedicPreceptor(num: string, first: string, last: string, active: boolean){
-    var docRef = db.collection('northshore').doc('preceptors').collection('paramedics');
+export function addParamedicPreceptor(num: string, first: string, last: string, active: boolean): any {
+  var docRef = db.collection('northshore').doc('preceptors').collection('paramedics');
 
-    var data = {
-        firstName: first, 
-        lastName: last,
-        active: true
-    };
-    
-    var setMedic = docRef.doc(num).set(data)
+  var data = {
+    firstName: first,
+    lastName: last,
+    active: true
+  };
+
+  var setMedic = docRef.doc(num).set(data)
 }
 
 /**
@@ -45,22 +45,22 @@ export function addParamedicPreceptor(num: string, first: string, last: string, 
  * 
  * @param {string} employeeId: employee number as STRING. must include leading zero. 
  * 
- * @returns {object} returns object that is in preceptor docuemtent ex: {active: TRUE, firstName: "John", lastName: "Smith"} 
+ * @returns {object} returns object that is in preceptor docuement ex: {active: TRUE, firstName: "John", lastName: "Smith"} 
  * If no preceptor found then returns undefinded. 
  */
-export function getParamedicPreceptor(employeeId){
-    var docRef = db.collection('northshore').doc('preceptors').collection('paramedics').doc(employeeId);  
-   
-    docRef.get().then(function(doc) {
-        if (doc.exists) {
-            return doc.data();
-        } else {
-            // doc.data() will be undefined in this case
-            return 'Doc not found'; 
-        }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
+export function getParamedicPreceptor(employeeId: string) {
+  var docRef = db.collection('northshore').doc('preceptors').collection('paramedics').doc(employeeId);
+
+  docRef.get().then(function (doc) {
+    if (doc.exists) {
+      return doc.data();
+    } else {
+      // doc.data() will be undefined in this case
+      return 'Doc not found';
+    }
+  }).catch(function (error) {
+    console.log("Error getting document:", error);
+  });
 }
 
 
@@ -74,33 +74,33 @@ export function getParamedicPreceptor(employeeId){
  * @returns {object} returns object that is in preceptor docuemtent ex: {active: TRUE, firstName: "John", lastName: "Smith"} 
  * If no preceptor found then returns undefinded. 
  */
-export function getEmtPreceptor(employeeId){
-    var docRef = db.collection('northshore').doc('preceptors').collection('emts').doc(employeeId);  
-    
-   docRef.get().then(function(doc) {
-        if (doc.exists) {
-            return doc.data();
-        } else {
-            // doc.data() will be undefined in this case
-            return undefined; 
-        }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
+export function getEmtPreceptor(employeeId) {
+  var docRef = db.collection('northshore').doc('preceptors').collection('emts').doc(employeeId);
+
+  docRef.get().then(function (doc) {
+    if (doc.exists) {
+      return doc.data();
+    } else {
+      // doc.data() will be undefined in this case
+      return undefined;
+    }
+  }).catch(function (error) {
+    console.log("Error getting document:", error);
+  });
 }
 
 
-export function getAllParamedicPreceptors(){
-    var medicRef = db.collection('northshore').doc('preceptors').collection('paramedics');
-    var allMedics = medicRef.get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          console.log(doc.id, '=>', doc.data());
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
+export function getAllParamedicPreceptors() {
+  var medicRef = db.collection('northshore').doc('preceptors').collection('paramedics');
+  var allMedics = medicRef.get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
       });
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
 
 }
 
@@ -111,37 +111,55 @@ export function getAllParamedicPreceptors(){
  * 
  * @returns {string[]}. returns an array of strings that are active preceptors. 
  */
- export async function getAllActivePreceptors(){
-    var medicRef = await db.collection('northshore').doc('preceptors').collection('paramedics');
-    var p = [];
-    var query = await medicRef.where('active', '==', true).get()
-      .then(snapshot => {
-        if (snapshot.empty) {
-          console.log('No matching documents.');
-          return;
-        }
-    
-        snapshot.forEach(doc => {
-          console.log(doc.id, '=>', doc.data());
-          p.push(doc.data());
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
+export async function getAllActivePreceptors() {
+  var medicRef = await db.collection('northshore').doc('preceptors').collection('paramedics');
+  var p = [];
+  var query = await medicRef.where('active', '==', true).get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        console.log('No matching documents.');
+        return;
+      }
+
+      snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+        p.push(doc.data());
       });
-    return p; 
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+  return p;
 }
 
-export function logIt(message: string){
-    console.log(message);
+export function logIt(message: string) {
+  console.log(message);
 }
 
 
 
-export async function process_tasks(employeeId: string) {
-  let docRef = await db.collection('northshore').doc('preceptors').collection('paramedics').doc(employeeId);
-  let snapshot = await docRef.get(); 
-   let final = snapshot.data();
-   return final; 
+export async function getAllActiveEMTPreceptors() {
+  let emtRef = await db.collection('northshore').doc('preceptors').collection('emts');
+  let activeEmts = await emtRef.where('active', '==', true).get();
+  let preceptors: string[] = [];
+  for (let emt of activeEmts.docs) {
+    preceptors.push(emt.id); 
+  }
+  return preceptors; 
 }
 
+export async function getAllActiveParamedicPreceptors() {
+  let emtRef = await db.collection('northshore').doc('preceptors').collection('paramedics');
+  let activeEmts = await emtRef.where('active', '==', true).get();
+  let preceptors: string[] = [];
+  for (let emt of activeEmts.docs) {
+    preceptors.push(emt.id); 
+  }
+  return preceptors;  
+}
+
+async function test(){
+  await getAllActiveParamedicPreceptors(); 
+}
+
+test(); 
