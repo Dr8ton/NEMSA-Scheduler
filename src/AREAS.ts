6c25c2959fdbc57337af43de673f0f3e6e9ca045
Drayton Kittel <Drayton.kittel@gmail.com>
2019-04-10 16:52:43 -0500

    converted to Array of Objects for iteration

--- src/AREAS.ts
+++ src/AREAS.ts
@@ -1,8 +1,11 @@
 export const AREAS = [{
-    name: "Northshore", //Based on CrewScheduler so I can use it in the Puppeteer. 
-    calendar: {
+    name: "northshore", //Based on CrewScheduler so I can use it in the Puppeteer. 
+    calendarIds: {
         paramedic: "jk6907osaor1ku6gnh3uput0gc@group.calendar.google.com",
         emt: "5u7af47v5pf8v165ae54r53h98@group.calendar.google.com"
+    },
+    crewscheduler: {
+        region: 9
     }
 }
 
